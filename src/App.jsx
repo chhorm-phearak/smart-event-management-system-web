8ac67b244689dadefa58322725f9f1abf9047a76
PHEARAK <phkchm911@gmail.com>
2025-12-17 20:15:11 +0700

    Build-Structure-Web

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="w-screen min-h-screen bg-blue-500">
+      <h1 className="text-red-500 font-bold">HELLO REACT JS 20026</h1>
+    </div>
+  );
+};
+
+export default App;
